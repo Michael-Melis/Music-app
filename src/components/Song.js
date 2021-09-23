99b354db21b1123bdd7ba92e3b74103bd129bd4f
Hamka169 <michael.melis169@gmail.com>
2021-09-23 17:12:14 +0200

    setup of music app/components done

---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Picture</h1>
+      <p>Song name</p>
+      <p>Song artist</p>
+    </div>
+  );
+};
+
+export default Song;
